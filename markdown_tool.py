cb7369f5d441b001eb7226da06d8b60d27d797d1
Artiom N <artiom14@yandex.ru>
2021-09-10 15:36:00 +0300

    Parameters were sorted

--- markdown_tool.py
+++ markdown_tool.py
@@ -11,6 +11,7 @@ from time import strftime
 from mimetypes import types_map
 
 from pkg.transformers.md.transformer import ArticleTransformer as MarkdownArticleTransformer
+from pkg.transformers.html.transformer import ArticleTransformer as HTMLArticleTransformer
 from pkg.image_downloader import ImageDownloader
 from pkg.www_tools import is_url, get_base_url, get_filename_from_url, download_from_url
 from pkg.formatters.simple import SimpleFormatter
@@ -103,22 +104,22 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('article_file_path_or_url', type=str,
                         help='path to the article file in the Markdown format')
-    parser.add_argument('-s', '--skip-list', default=None,
-                        help='skip URL\'s from the comma-separated list (or file with a leading \'@\')')
-    parser.add_argument('-d', '--images-dirname', default='images',
-                        help='Folder in which to download images')
-    parser.add_argument('-p', '--images-publicpath', default='',
-                        help='Public path to the folder of downloaded images')
-    parser.add_argument('-a', '--skip-all-incorrect', default=False, action='store_true',
-                        help='skip all incorrect images')
-    parser.add_argument('-t', '--downloading-timeout', type=float, default=-1,
-                        help='how many seconds to wait before downloading will be failed')
     parser.add_argument('-D', '--dedup-with-hash', default=False, action='store_true',
                         help='Deduplicate images, using content hash')
-    parser.add_argument('-R', '--remove-source', default=False, action='store_true',
-                        help='Remove or replace source file')
+    parser.add_argument('-d', '--images-dirname', default='images',
+                        help='Folder in which to download images')
+    parser.add_argument('-a', '--skip-all-incorrect', default=False, action='store_true',
+                        help='skip all incorrect images')
+    parser.add_argument('-s', '--skip-list', default=None,
+                        help='skip URL\'s from the comma-separated list (or file with a leading \'@\')')
     parser.add_argument('-o', '--output-format', default=out_format_list[0], choices=out_format_list,
                         help='output format')
+    parser.add_argument('-p', '--images-publicpath', default='',
+                        help='Public path to the folder of downloaded images')
+    parser.add_argument('-R', '--remove-source', default=False, action='store_true',
+                        help='Remove or replace source file')
+    parser.add_argument('-t', '--downloading-timeout', type=float, default=-1,
+                        help='how many seconds to wait before downloading will be failed')
     parser.add_argument('--output-path', type=str, help='article output file name')
     parser.add_argument('--version', action='version', version=f'%(prog)s {__version__}', help='return version number')
 
