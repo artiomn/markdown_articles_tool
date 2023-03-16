7d34b503a0ef6fc86640b0837263cf3d63358409
Artiom N <artiom@artiom>
2023-03-16 04:07:06 +0300

    Stop methods were added

--- markdown_toolset/transformers/__init__.py
+++ markdown_toolset/transformers/__init__.py
@@ -1,8 +1,6 @@
 from .md.transformer import ArticleTransformer as MarkdownArticleTransformer
 from .html.transformer import ArticleTransformer as HTMLArticleTransformer
 
-from .helpers import transform_article
-
 
 TRANSFORMERS = [MarkdownArticleTransformer, HTMLArticleTransformer]
-__all__ = [TRANSFORMERS, transform_article]
+__all__ = [TRANSFORMERS]
