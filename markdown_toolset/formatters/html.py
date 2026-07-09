3855a313dd0ba5ab7d6ae6fc36b78ea6e7d186d7
Artiom N. <artiom@artiom>
2026-07-10 00:43:33 +0300

    HTML code hl fixed

--- markdown_toolset/formatters/html.py
+++ markdown_toolset/formatters/html.py
@@ -3,6 +3,21 @@ HTML formatter.
 """
 
 from markdown import markdown
+from pygments.formatters import HtmlFormatter
+from markdown.extensions.codehilite import CodeHiliteExtension
+
+
+class CustomHtmlFormatter(HtmlFormatter):
+    def __init__(self, lang_str='', **options):
+        super().__init__(**options)
+        # lang_str has the value {lang_prefix}{lang}
+        # specified by the CodeHilite's options
+        self.lang_str = lang_str
+
+    def _wrap_code(self, source):
+        yield 0, f'<code class="{self.lang_str}">'
+        yield from source
+        yield 0, '</code>'
 
 
 class HTMLFormatter:
@@ -16,5 +31,9 @@ class HTMLFormatter:
     def write(lines, **kwargs):
         del kwargs
 
-        md = markdown(lines, output_format='html', extensions=['fenced_code', 'tables', 'codehilite'])
-        return f'<html>\n<head></head>\n<body>\n{md}\n</body>\n</html>'.encode()
+        html_formatter = CustomHtmlFormatter()
+        md = markdown(lines, output_format='html', extensions=['fenced_code',
+                                                               CodeHiliteExtension(pygments_formatter=html_formatter),
+                                                               'tables', 'toc'])
+        return f'<html>\n<head><style>\n{html_formatter.get_style_defs()}\n</style></head>\n' \
+               f'<body>\n{md}\n</body>\n</html>'.encode()
